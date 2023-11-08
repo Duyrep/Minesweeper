2de3bfc2e3103c16247ff5439925f43aa4e3e03e
Duyrep <131185997+Duyrep@users.noreply.github.com>
2023-11-08 17:56:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Main:
   def __init__(self) -> None:
     pg.init()
     cellSize = 30
-    gridSize = (9, 9)
+    gridSize = (30, 16)
     self.display = pg.display.set_mode((
       cellSize * gridSize[0] + 12 * 2,
       cellSize * gridSize[1] + 96 + 12
